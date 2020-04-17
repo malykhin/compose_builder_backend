ef90e0f299e5782d3b7bc58e2d59bec3d528b8f1
Denys Malykhin <denys.malykhin@solidopinion.com>
2020-04-17 04:37:40 +0300

    fix

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/ubuntu/app/node_modules/forever/bin/forever start /home/ubuntu/app/index.js
+sudo /home/ubuntu/app/node_modules/forever/bin/forever start /home/ubuntu/app/index.js
